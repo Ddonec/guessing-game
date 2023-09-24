5266339f70bea071930df38e5213fa955a354aaf
Anton <137609245+Ddonec@users.noreply.github.com>
2023-09-24 21:06:03 +0300

    feat: compledet task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,14 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  constructor() {}
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
+  guess() {
+    let number = Math.round((this.max + this.min) / 2);
+    return number;
 }
-
+  lower()   {this.setRange(this.min, this.guess())}
+  greater() {this.setRange(this.guess(), this.max)}
+}
 module.exports = GuessingGame;
